69742fa5d3fd33ab20fadcc891f7544647f0f7b4
許瑋軒 <3b232020@gm.student.ncut.edu.tw>
2026-03-16 17:11:09 +0800

    導覽列版面修改

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,12 +7,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <i class="bi bi-globe-americas text-2xl text-green-600"></i>
-                    </a>
-                </div>
+
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
